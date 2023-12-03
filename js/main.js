118146ed49794227fa3241d8924a35bcaded0d56
Sergey E <evilx.emelyanov@gmail.com>
2023-12-03 22:44:41 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const smiles = document.querySelectorAll('.smile > i');
+
+function switchSmileColorHandler(event) {
+    event.target.classList.toggle('fa-regular');
+    event.target.classList.toggle('fa-solid');
+}
+
+function counterIncrementHandler(event) {
+    const counter = event.target.nextElementSibling;
+
+    counter.textContent = String(parseInt(counter.textContent) + 1);
+}
+
+document.querySelectorAll('.smile > i').forEach(s => {
+    s.addEventListener('mouseenter', switchSmileColorHandler);
+    s.addEventListener('mouseleave', switchSmileColorHandler);
+    s.addEventListener('click', counterIncrementHandler);
+});
