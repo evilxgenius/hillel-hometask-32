f6707a223987ea43d9007ef5aa3a6bd8cc02dbd7
Sergey E <43378521+evilxgenius@users.noreply.github.com>
2023-12-04 19:58:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const smiles = document.querySelectorAll('.smile > i');
-
 function switchSmileColorHandler(event) {
     event.target.classList.toggle('fa-regular');
     event.target.classList.toggle('fa-solid');
@@ -8,7 +6,7 @@ function switchSmileColorHandler(event) {
 function counterIncrementHandler(event) {
     const counter = event.target.nextElementSibling;
 
-    counter.textContent = String(parseInt(counter.textContent) + 1);
+    counter.textContent = String(+counter.textContent + 1);
 }
 
 document.querySelectorAll('.smile > i').forEach(s => {
